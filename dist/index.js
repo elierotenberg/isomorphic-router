3390668fee807c6926b6b80a469dcbc81426c750
Clément Fiorio <clement.fiorio@gmail.com>
2015-04-28 14:50:59 +0200

    fixed eslint

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,26 @@
 'use strict';
 
-var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+var _interopRequireDefault = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
 
-var _prototypeProperties = function (child, staticProps, instanceProps) { if (staticProps) Object.defineProperties(child, staticProps); if (instanceProps) Object.defineProperties(child.prototype, instanceProps); };
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-require("babel/polyfill");
-var _ = require("lodash");
-var should = require("should");
-var Promise = (global || window).Promise = require("bluebird");
-var __DEV__ = process.env.NODE_ENV !== "production";
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _parse = require('url');
+
+var _pathToRegExp = require('path-to-regexp');
+
+var _pathToRegExp2 = _interopRequireDefault(_pathToRegExp);
+
+require('babel/polyfill');
+var _ = require('lodash');
+var should = require('should');
+var Promise = (global || window).Promise = require('bluebird');
+var __DEV__ = process.env.NODE_ENV !== 'production';
 var __PROD__ = !__DEV__;
 var __BROWSER__ = typeof window === 'object';
 var __NODE__ = !__BROWSER__;
@@ -18,11 +28,9 @@ if (__DEV__) {
   Promise.longStackTraces();
   Error.stackTraceLimit = Infinity;
 }
-var parse = require("url").parse;
-var pathToRegExp = _interopRequire(require("path-to-regexp"));
 
 function decodeParam(val) {
-  if (typeof val !== "string") {
+  if (typeof val !== 'string') {
     return val;
   }
   return decodeURIComponent(val);
@@ -31,6 +39,7 @@ function decodeParam(val) {
 var Router = (function () {
   // A pattern is an express-like path pattern.
   // It typically is like /users/:user/* or the like.
+
   function Router() {
     _classCallCheck(this, Router);
 
@@ -38,89 +47,80 @@ var Router = (function () {
     this._routes = [];
   }
 
-  _prototypeProperties(Router, null, {
-    clear: {
-      value: function clear() {
-        this._routes = [];
-      },
-      writable: true,
-      configurable: true
-    },
-    on: {
-      value: function on(pattern, fn) {
-        if (__DEV__) {
-          pattern.should.be.a.String;
-          fn.should.be.a.Function;
-        }
-
-        var keys = [];
-        var re = pathToRegExp(pattern, keys);
-        var len = keys.length;
-
-        this._routes.push(function (url) {
-          var u = parse(url, true);
-          var pathname = u.pathname;
-          var hash = u.hash;
-          var query = u.query ? u.query : {};
-          var match = re.exec(pathname);
-          if (match === null) {
-            return null;
-          }
-          match.shift();
-          var params = {};
-          for (var k = 0; k < len; k = k + 1) {
-            params[keys[k].name] = decodeParam(match[k]);
-          }
-          return fn(params, query, hash);
-        });
-
-        return this;
-      },
-      writable: true,
-      configurable: true
-    },
-    route: {
-      value: function route(url) {
-        if (__DEV__) {
-          url.should.be.a.String;
-        }
-        var results = [];
-        _.each(this._routes, function (route) {
-          var res = route(url);
-          if (res !== null) {
-            results.push(res);
-          }
-        });
-        return results;
-      },
-      writable: true,
-      configurable: true
-    },
-    routeRequest: {
-      value: function routeRequest(req) {
-        if (__DEV__) {
-          req.should.be.an.Object.which.has.property("url").which.is.a.String;
-          __NODE__.should.be["true"];
-        }
-        return this.route(req.url);
-      },
-      writable: true,
-      configurable: true
-    },
-    routeWindow: {
-      value: function routeWindow(window) {
-        if (__DEV__) {
-          window.should.be.an.Object;
-          __BROWSER__.should.be["true"];
-        }
-        return this.route((window.location || window.history.location).href);
-      },
-      writable: true,
-      configurable: true
+  _createClass(Router, [{
+    key: 'clear',
+    value: function clear() {
+      this._routes = [];
     }
-  });
+  }, {
+    key: 'on',
+    value: function on(pattern, fn) {
+      if (__DEV__) {
+        pattern.should.be.a.String;
+        fn.should.be.a.Function;
+      }
+
+      var keys = [];
+      var re = _pathToRegExp2['default'](pattern, keys);
+      var len = keys.length;
+
+      this._routes.push(function (url) {
+        var u = _parse.parse(url, true);
+        var pathname = u.pathname;
+        var hash = u.hash;
+
+        var query = u.query ? u.query : {};
+        var match = re.exec(pathname);
+        if (match === null) {
+          return null;
+        }
+        match.shift();
+        var params = {};
+        for (var k = 0; k < len; k = k + 1) {
+          params[keys[k].name] = decodeParam(match[k]);
+        }
+        return fn(params, query, hash);
+      });
+
+      return this;
+    }
+  }, {
+    key: 'route',
+    value: function route(url) {
+      if (__DEV__) {
+        url.should.be.a.String;
+      }
+      var results = [];
+      _.each(this._routes, function (route) {
+        var res = route(url);
+        if (res !== null) {
+          results.push(res);
+        }
+      });
+      return results;
+    }
+  }, {
+    key: 'routeRequest',
+    value: function routeRequest(req) {
+      if (__DEV__) {
+        req.should.be.an.Object.which.has.property('url').which.is.a.String;
+        __NODE__.should.be['true'];
+      }
+      return this.route(req.url);
+    }
+  }, {
+    key: 'routeWindow',
+    value: function routeWindow(window) {
+      if (__DEV__) {
+        window.should.be.an.Object;
+        __BROWSER__.should.be['true'];
+      }
+      return this.route((window.location || window.history.location).href);
+    }
+  }]);
 
   return Router;
 })();
 
-module.exports = Router;
+exports['default'] = Router;
+module.exports = exports['default'];
--- src/test.js
+++ src/test.js
@@ -6,13 +6,14 @@ let user1 = 0;
 let queryFooBar = 0;
 let hashFoo = 0;
 
+/* eslint-disable no-unused-vars */
 const router = new Router()
 .on('/(.*)', () => catchAll = catchAll + 1)
 .on('/users', () => users = users + 1)
 .on('/users/:user', (params) => user1 = user1 + (params.user === '1' ? 1 : 0))
 .on('/query', (params, query) => queryFooBar = queryFooBar + (query.foo && query.foo === 'bar' ? 1 : 0))
 .on('/hash', (params, query, hash) => hashFoo = hashFoo + (hash === '#foo' ? 1 : 0));
-
+/* eslint-enable no-unused-vars */
 
 router.route('/hello');
 catchAll.should.be.exactly(1);
